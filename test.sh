f2386d43b0d149261f6dd36aeee771deee7e6b5c
Christian <christian@klauenboesch.ch>
2016-03-10 11:33:49 +0100

    added +x flag to scripts, improved test

--- test.sh
+++ test.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
+command_exists () {
+    type "$1" &> /dev/null ;
+}
+
+if ! export | grep -Eq '^declare -x ATLAS_TOKEN=' ; then
+    echo "You need to export the ATLAS_TOKEN"
+    exit 1
+fi
+
+if ! command_exists packer; then
+    echo "Packer is not installed"
+    exit 1
+fi
+if ! command_exists vagrant; then
+    echo "Vagrant is not installed"
+    exit 1
+fi
+if ! command_exists virtualbox; then
+    echo "Virtualbox is not installed"
+    exit 1
+fi
+
 rm -rf output-virtualbox-iso
 packer build "$1.json"
 vagrant box add packer_virtualbox-iso_virtualbox.box --name="tmp/$1" --force
