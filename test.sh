b28f16e98bf8efa845d3832f06aabfc052d33d33
Christian Klauebösch <christian@klauenboesch.ch>
2016-03-10 09:58:38 +0100

    minor fixes

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ rm -rf output-virtualbox-iso
 packer build "$1.json"
 vagrant box add packer_virtualbox-iso_virtualbox.box --name="tmp/$1" --force
 cd test
+rm -rf ./.sql_dumps
 vagrant destroy -f
 vagrant box update
 vagrant up $1
